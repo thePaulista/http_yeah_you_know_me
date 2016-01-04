afad9e002274ea0d7d97859d0ebf15cf05062a12
Hedy <hedywoo@gmail.com>
2016-01-04 14:45:23 -0700

    initial commit for server pproject

---
+++ lib/hello_server.rb
@@ -0,0 +1,22 @@
+require 'socket'
+require 'pry'
+
+class Hello_Server
+
+  def initialize(x = "localhost", y = 9292)
+    @server = TCPServer.new(x, y)
+    @counter = 0
+  end
+
+  def runner
+    while socket = @server.accept
+      request = socket.gets
+      response1 = "Ola Planeta Azul (#{@counter+=1})"
+      socket.puts response1
+      socket.close
+     end
+   end
+end
+
+server = Hello_Server.new
+server.runner
---
+++ test/hello_server.rb
@@ -0,0 +1,36 @@
+require 'test_helper'
+require 'hello_server'
+
+class Hello_ServerTest < Minitest::Test
+
+  def test_TCPServer_can_be_initialized
+    skip
+    server = Hello_Server.new
+
+  end
+
+  def test_localhost_is_the_default_host
+    skip
+    server = Hello_Server.new
+
+  end
+
+  def test_that_9292_is_the_default_port
+    skip
+    server = Hello_Server.new
+
+  end
+
+  def test_that_counter_is_set_to_zero_
+    skip
+    server = Hello_Server.new
+
+  end
+
+  def test_that_counter_can_go_incrementally_up
+    skip
+    server = Hello_Server.new
+
+  end
+
+end
