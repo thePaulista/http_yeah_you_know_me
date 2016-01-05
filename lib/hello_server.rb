7a2f1ecec053a350b5853df6a9e6b26fb36b4bea
Hedy <hedywoo@gmail.com>
2016-01-04 22:06:44 -0700

    debugging variables

--- lib/hello_server.rb
+++ lib/hello_server.rb
@@ -12,8 +12,8 @@ class Hello_Server
     while client_socket = @server.accept
       request = client_socket.gets
       response = "Ola Planeta Azul (#{@counter+=1})"
-      socket.puts response
-      socket.close
+      client_socket.puts response
+      client_socket.close
      end
    end
 end
