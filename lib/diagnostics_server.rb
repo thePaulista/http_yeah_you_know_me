be6d570ee2a6c08292e7f5d6b32c85e3466d4f45
Hedy <hedywoo@gmail.com>
2016-01-04 22:33:47 -0700

    initial commit for iter 1

---
+++ lib/diagnostics_server.rb
@@ -0,0 +1,30 @@
+require 'socket'
+
+class Diagnostics_Server
+
+  def initialize(host = "localhost", port = 9292)
+    @server = TCPServer.new(host, port)
+  end
+
+  def run
+    request_lines = ["Verb: POST",
+                    "Path: /",
+                    "Protocol: HTTP/1.1",
+                    "Host: localhost",
+                    "Port: 9292",
+                    "Origin: 127.0.0.1",
+                    "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
+
+    ]
+    while client_socket = @server.accept
+      request = client_socket.gets
+      response = "<pre>" + request_lines.join("\n") + "</pre>"
+      output = "<html><head></head><body>#{response}</body></html>"
+      client_socket.puts output
+      client_socket.close
+    end
+  end
+end
+
+server = Diagnostics_Server.new
+server.run
